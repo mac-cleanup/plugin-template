3dcbf418237e98b104c9c34234a866404c1a7f95
Florian Wartner <fwartner@users.noreply.github.com>
2021-08-17 23:05:04 +0200

    Update plugin.go

--- src/plugin.go
+++ src/plugin.go
@@ -1,11 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 type plugin string
 
+var introMsg = "Cleaning up MY_FILES.." // This is the message that will be displayed to the user
+
 func (g plugin) Cleanup() {
-	fmt.Println("Hello Universe")
+	fmt.Println(introMsg)
+	DeleteFiles("~/DIR_YOU_WANT_TO_BE_CLEANED") // DIR_YOU_WANT_TO_BE_CLEANED is the path to the directory you want to be cleaned
+}
+
+func DeleteFiles(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 var Cleanup plugin
